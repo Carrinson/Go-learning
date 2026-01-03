ba32c81a1b9feabd3397305f5e7cc3dbfd2ba5c7
Carrinson <folacarrinson@gmail.com>
2026-01-03 16:31:56 +0100

    maps

--- main.go
+++ main.go
@@ -2,47 +2,9 @@ package main
 
 import "log"
 
-type User struct{
-	FirstName string 
-	LastName string
-}
- 
+
 func main() {
-		// Wrong way to create a map
-	// var myOtherMap map[string]string
+
 	
-								// String
-	// myMap := make(map[string]string)
 
-	// myMap["dog"] = "Samson"
-	// myMap["other-dog"] = "Cassie" 
-
-	// log.Println(myMap["dog"])
-	// log.Println(myMap["other-dog"])
-
-
-								// int
-	// myMap := make(map[string]int)
-	// myMap["First"] = 1
-	// myMap["Second"] = 2
-
-	// log.Println(myMap["First"])
-	// log.Println(myMap["Second"])
-
-
-								// Types (Structs)
-	myMap := make(map[string]User)
-
-	me := User {
-		FirstName: "Trevor",
-		LastName: "Sawler",
-	}
-
-	myMap["me"] = me
-
-	log.Println(myMap["me"].FirstName)
-	log.Println(myMap["me"].LastName)
-
-	var myNewVar float32
-	myNewVar = 11.1
 }
